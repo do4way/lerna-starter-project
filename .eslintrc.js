af61c66e5ae2ce1007a15df3c661b95b7ea50d72
Yongwei <yongwei.dou@gmail.com>
2024-07-02 08:26:38 +0900

    Project initialization.

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+	  project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	extends: [
+	  'eslint:recommended',
+	  'plugin:@typescript-eslint/recommended',
+	  'plugin:prettier/recommended',
+	],
+	rules: {
+	  'prettier/prettier': 'error',
+	},
+  };
